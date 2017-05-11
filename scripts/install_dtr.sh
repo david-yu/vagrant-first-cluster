0b63a93a7501f175644afec621f7e5d13dfe96bd
David Yu <david.yu@docker.com>
2017-05-11 14:03:14 -0700

    changes to dtr install

--- scripts/install_dtr.sh
+++ scripts/install_dtr.sh
@@ -21,9 +21,9 @@ docker run --rm docker/dtr:2.2.4 install --hub-username ${HUB_USERNAME} --hub-pa
 # Run backup of DTR
 docker run --rm docker/dtr:2.2.4 backup --ucp-url https://${UCP_IPADDR} --existing-replica-id ${DTR_REPLICA_ID} --ucp-username admin --ucp-password ${UCP_PASSWORD} --ucp-ca "$(cat ucp-ca.pem)" > /tmp/backup.tar
 # Trust self-signed DTR CA
-openssl s_client -connect ${DTR_IPADDR}:443 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee /usr/local/share/ca-certificates/${DTR_IPADDR}.crt
-sudo update-ca-certificates
-sudo service docker restart
+curl -k https://dtr.local/ca -o /etc/pki/ca-trust/source/anchors/dtr.local.crt
+sudo update-ca-trust
+sudo /bin/systemctl restart docker.service
 # Copy convenience scripts
 sudo cp -r /vagrant/scripts /home/ubuntu/scripts
 # Download UCP client bundle
